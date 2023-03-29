24879e3547ce5875a846c38b78f68e4b155b2f54
ArtomDovgal <artomdovgalnew@gmail.com>
2023-03-29 14:40:32 +0300

    change -for-lab4

--- app.js
+++ app.js
@@ -1,89 +1,120 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const MongoClient = require("mongodb").MongoClient;
-const { ObjectId } = require("mongodb");
-
+const Schema = mongoose.Schema;
 const app = express();
 const jsonParser = express.json();
-const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/", { useUnifiedTopology: true });
- 
-let dbClient;
- 
+
+
+const passengerScheme = new Schema(
+    {firstname: String,
+    lastname: String,
+    age: Number,
+    phone: Number,
+    type: {
+        type: String,
+        enum : ['Common', 'Business','Luxury'],
+        default: 'Common'
+    } 
+    }, {versionKey: false});
+
+const Passenger = mongoose.model("Passenger", passengerScheme);
+
 app.use(express.static(__dirname + "/public"));
- 
-mongoClient.connect().then(mongoClient => 
-    {
-        dbClient = mongoClient;
-        const tmp = app.locals.collection = dbClient.db("passengersdb").collection("passengers");
-        app.listen(3000, function(){
-            console.log("Сервер очікує підключення...");
-        });
-    });
- 
+
+mongoose.connect("mongodb://127.0.0.1:27017/passengersdb", { useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => {
+app.listen(3000, function(){
+console.log("Сервер ожидает подключения...");
+});
+})
+.catch((err) => {
+console.log(err);
+});
+
 app.get("/api/passengers", function(req, res){
         
-    const collection = req.app.locals.collection;
-    collection.find().toArray().then((passengers) => {
-        res.json(passengers)
-    }).catch((err) => console.log(err));
-     
-});
-app.get("/api/passengers/:id", function(req, res){
-        
-    const id = new ObjectId(req.params.id);
-    const collection = req.app.locals.collection;
-
-    collection.findOne({_id: id}).then(function(passenger){
-        res.send(passenger);
-    }).catch((err) => console.log(err));
-});
-   
-app.post("/api/passengers", jsonParser, function (req, res) {
-       
-    if(!req.body) return res.sendStatus(400);
-       
-    const passengerFirstName = req.body.firstname;
-    const passengerLastName = req.body.lastname;
-    const passengerAge = req.body.age;
-    const passengerPhone = req.body.phone;
-    const passengerType = req.body.type;
-    const passenger = {firstname: passengerFirstName, lastname: passengerLastName, age: passengerAge, phone: passengerPhone, type: passengerType};
-       
-    const collection = req.app.locals.collection;
-    collection.insertOne(passenger).then(function(result){
-        res.json(passenger);
-    }).catch((err) => console.log(err));
-});
-    
-app.delete("/api/passengers/:id", function(req, res){
-        
-   const id = new ObjectId(req.params.id);
-    const collection = req.app.locals.collection;
-
-    collection.findOneAndDelete({_id: id}).then(function(result){    
-        let passenger = result.value;
-        res.send(passenger);
-    }).catch((err) => console.log(err));
-});
-   
-app.put("/api/passengers", jsonParser, function (req, res) {
-
-    if (!req.body) return res.sendStatus(400);
-    const id = new ObjectId(req.body.id);
-    const passengerFirstName = req.body.firstname;
-    const passengerLastName = req.body.lastname;
-    const passengerAge = req.body.age;
-    const passengerPhone = req.body.phone;
-    const passengerType = req.body.type;
-
-    const collection = req.app.locals.collection;
-    collection.findOneAndUpdate({ _id: id }, 
-        { $set: {firstname: passengerFirstName, lastname: passengerLastName, age: passengerAge, phone: passengerPhone, type: passengerType} },
-        { returnNewDocument: true },).then(function(result){   
-            const  passenger = result.returnNewDocument;
-            res.send(passenger);
-        }).catch((err) => console.log(err));
+    Passenger.find({})
+    .then((users) => {
+        res.send(users);
+    })
+    .catch((err) => {
+        console.log(err);
     });
- 
+});
+   
+app.get("/api/passengers/:id", function(req, res){
+         
+    const id = req.params.id;
+    Passenger.findOne({_id: id})
+        .then((passenger) => {
+            res.send(passenger);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.post("/api/passengers", jsonParser, function (req, res) {
+        
+    if(!req.body) return res.sendStatus(400);
+    
+    const passengerFirstName = req.body.firstname;
+    const passengerLastName = req.body.lastname;
+    const passengerAge = req.body.age;
+    const passengerPhone = req.body.phone;
+    const passengerType = req.body.type;
+    const passenger = new Passenger({firstname: passengerFirstName, lastname: passengerLastName,
+    age: passengerAge, phone: passengerPhone, type: passengerType});
+        
+    passenger.save()
+        .then(() => {
+            res.send(passenger);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+app.delete("/api/passengers/:id", function(req, res){
+         
+    const id = req.params.id;
+    Passenger.findByIdAndDelete({_id: id})
+        .then((passenger) => {
+            res.send(passenger);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+         
+    app.put("/api/passengers", jsonParser, function(req, res){
+
+            if(!req.body) return res.sendStatus(400);
+            const id = req.body.id;
+            const passengerFirstName = req.body.firstname;
+            const passengerLastName = req.body.lastname;
+            const passengerAge = req.body.age;
+            const passengerPhone = req.body.phone;
+            const passengerType = req.body.type;
+            const updateFields = {
+                firstname: passengerFirstName, 
+                lastname: passengerLastName,
+                age: passengerAge, 
+                phone: passengerPhone, 
+                type: passengerType
+            };
+        
+            Passenger.findOneAndUpdate({_id: id}, updateFields, {new: true})
+                .then((passenger) => {
+                    res.send(passenger);
+                })
+                .catch((err) => {
+                    console.log(err); 
+                });
+        });
+    
+
 process.on("SIGINT", () => {
     dbClient.close();
     process.exit();
